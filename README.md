b7ff2c604c2dcb952e86398f7f19e3da306c7058
ChatGTHB <cihat.kose@hotmail.com>
2024-03-12 20:33:50 +0100

    Edited

--- README.md
+++ README.md
@@ -13,3 +13,5 @@
  ## Windows
  ## Scroll
  ## Robot
+ ## Record Screen
+ ## Windows Size
--- src/_11_RecordScreen_WindowsSize/SS_WindowSize.java
+++ src/_11_RecordScreen_WindowsSize/SS_WindowSize.java
@@ -1,4 +1,4 @@
-package src._11_EkranKaydetme_WindowsSize;
+package src._11_RecordScreen_WindowsSize;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.util.List;
 
 import static src.utility.BaseDriver.driver;
-import static src.utility.BaseDriver.wait;
 
 public class SS_WindowSize {
     @Test
